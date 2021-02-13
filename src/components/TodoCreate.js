7c8fbf5ae5a108f981c00cea33b4c0aa5dde31ac
Jengyoung <young_pallete@naver.com>
2021-02-13 13:22:15 +0900

    Input UI 구현

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -31,14 +31,66 @@ const CircleButton = styled.button`
     
     border: none;
     outline: none;
+
+    transition: 0.125s all ease-in;
+    ${props => props.open && css`
+        background: #ff6b6b;
+        &:hover {
+            background: #ff8787;
+        }
+        &:active {
+            background: #fa5252;
+        }
+        transform: translate(-50%, 50%) rotate(45deg);
+    `
+    }
 `;
 
+// CircleButton click시, form이 보이도록 설정.
+const InsertFormPositioner = styled.div`
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    position: absolute;
+`;
+
+const InsertForm = styled.div`
+    background: #f8f9fa;
+    padding: 32px;
+    padding-bottom: 72px;
+    padding-bottom-left-radius: 16px;
+    border-bottom-right-radius: 16px;
+    border-top: 1px solid #e9ecef;
+    
+`;
+
+const Input = styled.input`
+    padding: 12px;
+    border-radius: 4px;
+    border: 1px solid #dee2e6;
+    width: 100%;
+    outline: none;
+    font-size: 18px;
+    box-sizing: border-box;
+`;
 
 function TodoCreate() {
+    const [ open, setOpen ] = useState(false);
+    const onToggle = () => setOpen(!open);
 
-    return <CircleButton>
-        <MdAdd />
-    </CircleButton>
+    return (
+        <>
+            {open && <InsertFormPositioner>
+                <InsertForm>
+                    <Input placeholder="할 일을 입력 후, Enter을 누르세요." autoFocus />
+                </InsertForm>
+                </InsertFormPositioner>}
+            <CircleButton onClick={onToggle} open={open}>
+                <MdAdd />
+            </CircleButton>
+        </>
+
+    )
 }
 
 export default TodoCreate;
